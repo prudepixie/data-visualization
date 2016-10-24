505a932147f6f060725fa2c03f189881632beeca
Wendy Hsu <wndyhsu@gmail.com>
2016-10-24 10:45:31 -0700

    parse data for recent week

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $( document ).ready(function() {
+    var lastYear
+      , lastMonth
+      , lastWeek
+      , lastWeekData
 
     function getData() {
       var url = "https://gist.githubusercontent.com/evanjacobs/c150c0375030dc4de65e9b95784dc894/raw/35c5f455b147703db3989df0cb90f5781c3b312f/usage_data.json";
       $.getJSON( url, {format: "json"})
       .done(function( data ) {
-        console.log(data);
         var items = [];
+        addDateAttributeToItems(data);
+        lastWeekData = getLastWeekData(data)
+        console.log("LAST WEEK SHITTT");
+        console.log(lastWeekData);
+        getDayOfWeek(lastWeekData);
+        var lastMonthData = getLastMonthData(data)
+        console.log("LAST MONTH SHITTT");
+        console.log(lastMonthData);
+        setChart();
         $.each( data, function(i, item) {
           var date = data[i].date;
           var users = data[i].users;
@@ -20,44 +32,91 @@ $( document ).ready(function() {
     };
     getData();
 
+    function setChart() {
+      console.log("LAST WEEEEEK");
+      console.log(lastWeekData);
+      var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+            label: "Latest Week",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+          }
+        ]
+      };
+      var ctx = document.getElementById("weeklyChart");
+      var weeklyChart = new Chart(ctx, {
+        type: 'line',
+        data: data
+      });
+    }
 
-    var ctx = document.getElementById("chartOne");
-    var chartOne = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    });
+    function getDayOfWeek(items) {
+      console.log("Week Day");
+      var weekDays = [];
+      weekDays = items.reverse().map(function(item){
+        return moment(item.date).format("dddd")
+      });
+      console.log(weekDays);
+    }
+    function addDateAttributeToItems(items) {
+      items.forEach(function(item){
+        item.week = moment(item.date, "YYYY-MM-DD").week()
+        item.month = moment(item.date, "YYYY-MM-DD").month()
+        item.year = moment(item.date, "YYYY-MM-DD").year()
+      });
+      lastYear = _.last( _.sortBy(items.map(function(item){
+        return item.year
+      }).reverse(), function(sortingItem){
+        return sortingItem.year
+      }));
+    };
+
+    function getLastWeekData(items){
+      lastWeek = _.last( _.sortBy(items.map(function(item){
+        return item.week
+      }).reverse(), function(sortingItem){
+        return sortingItem.week
+      }))
+
+      filteredItems = items.filter(function(item){
+        return item.year == lastYear && item.week == lastWeek
+      })
+
+      return filteredItems
+    }
+
+    function getLastMonthData(items){
+      lastMonth = _.last(_.sortBy(items.map(function(item){
+        return item.month
+      }).reverse(), function(sortingItem){
+        return sortingItem.month
+      }))
+
+      filteredItems = items.filter(function(item){
+        return item.year == lastYear && item.month == lastMonth
+      })
+
+      return filteredItems
+    }
 
 
 });
